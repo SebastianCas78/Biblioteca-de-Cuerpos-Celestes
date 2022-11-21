d46d92287700b2ff4e3a40dc48eeb669a7e44c49
SebastianCas78 <117695171+SebastianCas78@users.noreply.github.com>
2022-11-21 00:01:12 -0600

    Add files via upload

---
+++ Administrador_de_Archivo.h
@@ -0,0 +1,8 @@
+class Administrador_de_Archivo{ //Se declara la clase Administrador de Archivo y sus atributos para guardar la información en int, float y string.
+    private:
+
+    public:
+        void guardarInfo(string);
+        void guardarInfo(int);
+        void guardarInfo(float);
+};
