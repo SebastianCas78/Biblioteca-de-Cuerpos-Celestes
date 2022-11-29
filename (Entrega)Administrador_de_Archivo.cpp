927246c3d73533445546c7532a8674a3cd7ac9c2
SebastianCas78 <117695171+SebastianCas78@users.noreply.github.com>
2022-11-29 09:23:06 -0600

    Delete (Entrega)Administrador_de_Archivo.cpp

--- (Entrega)Administrador_de_Archivo.cpp
+++
@@ -1,7 +0,0 @@
-#include "Administrador_de_Archivo.h" //Se definen las funciones de la clase Administrador de Archivo.
-#include <sstream>
-#include "string.h"
-
-Administrador_Archivo::Administrador_Archivo(){
-}
-
