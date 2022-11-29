63437247368c993f62acc111097a31033de92c9e
SebastianCas78 <117695171+SebastianCas78@users.noreply.github.com>
2022-11-29 09:19:53 -0600

    Update Asteroide.cpp

--- Asteroide.cpp
+++ Asteroide.cpp
@@ -1,5 +1,18 @@
 #include "Asteroide.h" //Se definen las funciones de la clase Asteroide.
+#include<sstream>
 
-Asteroide Asteroide(float diametro_){
-    diametro=diametro_;
+Asteroide::Asteroide(){
+    diametro=0;
+}
+
+Asteroide::Asteroide(bool _tipo_Movimiento,bool _orbita_Cuerpo,bool _visibles,int _diametro):Cuerpo_Celeste(_tipo_Movimiento,_orbita_Cuerpo,_visibles){
+    diametro=_diametro;
+}
+
+void Asteroide::setDiametro(int _diametro){
+    diametro=_diametro;
+}
+
+int Asteroide::getDiametro(){
+    return diametro;
 }
