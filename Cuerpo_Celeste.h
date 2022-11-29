d7c8528b2d361c1278a938c73adb4a63362775fa
SebastianCas78 <117695171+SebastianCas78@users.noreply.github.com>
2022-11-29 09:10:16 -0600

    Update Cuerpo_Celeste.h

--- Cuerpo_Celeste.h
+++ Cuerpo_Celeste.h
@@ -1,9 +1,29 @@
-class Cuerpo_Celeste{
-    private:
-        string cuentanConAlgunTipoDeMovimiento;
-        string orbitaAlgunCuerpo;
-        string visiblesDesdeElPlaneta;
+#ifndef CUERPO_CELESTE
+#define CUERPO_CELESTE
+#include "string.h"
+using namespace std;
 
-    public:
-       string imprimeDatos();
+class Cuerpo_Celeste{
+    private: //Atributos
+        bool tipoMovimiento;
+        bool orbitaCuerpo;
+        bool visibles;
+
+    public: //Metodos
+        Cuerpo_Celeste();
+        Cuerpo_Celeste(bool, bool, bool);
+
+        //Setters
+        void setTipoMovimiento(bool);
+        void setOrbitaCuerpo(bool);
+        void setVisibles(bool);
+
+        //Getters
+        bool getTipoMovimiento();
+        bool getOrbitaCuerpo();
+        bool getVisibles();
+
+        //Superstring
+       //string imprimeDatos();
 };
+#endif
