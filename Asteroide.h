48ecf30fd3fc8b81e8bedf9f967f54907a73e771
SebastianCas78 <117695171+SebastianCas78@users.noreply.github.com>
2022-11-29 09:18:37 -0600

    Update Asteroide.h

--- Asteroide.h
+++ Asteroide.h
@@ -1,7 +1,18 @@
-class Asteroide{ //Se declara la clase asteroide y su atributo di·metro.
-    private:
-        float diametro;
+#include "string.h"
+#include "Cuerpo_Celeste.h"
+using namespace std;
 
-    public:
-        float imprimeDatos();
+class Asteroide : public Cuerpo_Celeste{//public Cuerpo_Celeste.h{ //Se declara la clase asteroide y su atributo di√°metro.
+    private: //Atributos
+        int diametro;
+
+    public: //Metodos
+        Asteroide();
+        Asteroide(bool,bool,bool,int);//Constructor
+
+        //Setters
+        void setDiametro(int);
+
+        //Getters
+        int getDiametro();
 };
