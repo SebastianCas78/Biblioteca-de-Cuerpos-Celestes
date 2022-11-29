d3991525d529decda6819f5c5aeb36ae734d40d0
SebastianCas78 <117695171+SebastianCas78@users.noreply.github.com>
2022-11-29 09:08:37 -0600

    Update Luna.cpp

--- Luna.cpp
+++ Luna.cpp
@@ -1,5 +1,17 @@
 #include "Luna.h" //Se definen las funciones de la clase Luna.
 
-Luna Luna(int cantidad_de_lunas_){
-    cantidad_de_lunas=cantidad_de_lunas_;
+Luna::Luna(){
+    diametro=0.0;
+}
+
+Luna::Luna(float _diametro){
+    diametro=_diametro;
+}
+
+void Luna::setDiametro(float _diametro){
+    diametro=_diametro;
+}
+
+float Luna::getDiametro(){
+    return diametro;
 }
