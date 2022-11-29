6af722afbefc72e27200fadd66a5bc7808a8eb45
SebastianCas78 <117695171+SebastianCas78@users.noreply.github.com>
2022-11-29 09:12:37 -0600

    Update Cometa.h

--- Cometa.h
+++ Cometa.h
@@ -1,8 +1,22 @@
-class Cometa{ //Se declara la clase Cometa y sus atributos tamaño de la cola y velocidad del cometa.
-    private:
-        float tamanoCola;
-        float velocidadCometa;
+#include "Cuerpo_Celeste.h"
+using namespace std;
 
-    public:
-        float imprimeDatos();
+class Cometa : public Cuerpo_Celeste{ //Se declara la clase Cometa y sus atributos tamaÅ„o de la cola y velocidad del cometa.
+    private: //Atributos
+        float tamanoCola, velocidadCometa;
+
+    public: //Metodos
+        Cometa();
+        Cometa(bool,bool,bool,float,float);
+
+        //Setters
+        void setTamanoCola(float);
+        void setVelocidadCometa(float);
+
+        //Getters
+        float getTamanoCola();
+        float getVelocidadCometa();
+
+        //Superstring
+        string imprimeDatos();
 };
