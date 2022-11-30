8272bba22ff397b2f406961caf191a2391cf6339
SebastianCas78 <117695171+SebastianCas78@users.noreply.github.com>
2022-11-30 10:27:30 -0600

    Update Biblioteca.cpp

--- Biblioteca.cpp
+++ Biblioteca.cpp
@@ -11,24 +11,25 @@
 using namespace std;
 
 int main(){
-    bool TipoMovimiento;
-    bool OrbitaCuerpo;
-    bool Visible;
-    Cuerpo_Celeste Cuerpo_Celeste1(true,true, true);
-    Asteroide Asteroide1(true,true, true,5000);
-    Cometa Cometa1(true,true, true,30000000,115000);
-    Planeta Planeta1(true,true, true,2,5.5,12742,365);
+    int TipoMovimiento;
+    int OrbitaCuerpo;
+    int Visible;
+    Cuerpo_Celeste Cuerpo_Celeste1(1,1,1);
+    Asteroide Asteroide1(1,1,1,5000);
+    Cometa Cometa1(1,1,1,30000000,115000);
+    Planeta Planeta1(1,1,1,2,5.5,12742,365);
     Luna Luna1(3474.8);
     Luna Luna2(3121.6);
 
     int op;
 
     while (true){
-    cout<<"Bienvenido a la Biblioteca de Cuerpos Celestes, ¿De qué Cuerpo Celeste quieres introducir información?."<<endl;
+    cout<<"Bienvenido a la Biblioteca de Cuerpos Celestes, ¿De que Cuerpo Celeste quieres introducir informacion?.\n"<<endl;
 
     int el;
 
     cout<<"Antes de empezar responde las siguientes preguntas (true/false):"<<endl;
+    cout<<"1 = Si, es cierto.\n2 = No, es falso.\n"<<endl;
     cout<<"1)Pregunta #1.\n2)Pregunta #2.\n3)Pregunta #3.\n4)Continuar.";cin>>el;
     if(el==1){
             cout<<"¿El Cuerpo Celeste tiene algún tipo de Movimiento?:";
@@ -36,28 +37,28 @@ int main(){
             cout<<"Es: "<<TipoMovimiento<<endl;
     }
     else if(el==2){
-            cout<<"¿El Cuerpo Celeste orbita algún otro Cuerpo?";
+            cout<<"¿El Cuerpo Celeste orbita algún otro Cuerpo?:";
             cin>>OrbitaCuerpo;
             cout<<"Es: "<<OrbitaCuerpo<<endl;
     }
     else if(el==3){
-            cout<<"¿El Cuerpo Celeste es visible a simple vista desde la Tierra?";
+            cout<<"¿El Cuerpo Celeste es visible a simple vista desde la Tierra?:";
             cin>>Visible;
             cout<<"Es: "<<Visible<<endl;
-            cout<<"El cuerpo Celeste cuenta con algún tipo de movimiento "<<TipoMovimiento<<" también orbita algun cuerpo "<<OrbitaCuerpo<<" y es visible desde el planeta "<<Visible<<endl;
+            cout<<"El cuerpo Celeste cuenta con algún tipo de movimiento "<<TipoMovimiento<<" también orbita algun cuerpo "<<OrbitaCuerpo<<" y es visible desde el planeta "<<Visible<<"\n"<<endl;
     }
     else if(el==4){
-            cout<<"1)Asteroide.\n2)Cometa.\n3)Planeta.\n4)Luna.\n5)Salir.";cin>>op;
+            cout<<"\n1)Asteroide.\n2)Cometa.\n3)Planeta.\n4)Luna.\n5)Salir.\n";cin>>op;
             if(op==1){
                 int op2;
-                cout<<"1)Ingresar datos. \n2) Usar datos predefinidos. \n3)Salir.";cin>>op2;
+                cout<<"1)Usar datos predefinidos. \n2)Ingresar datos. \n3)Salir.\n";cin>>op2;
                 if(op2==1){
-                        cout<<Asteroide1.getDiametro()<<endl;
+                        cout<<"El diametro del asteroide es de: "<<Asteroide1.getDiametro()<<" km.\n"<<endl;
                 }
                 else if(op2==2){
                         int setDiametro;
                         cout<<"Introduce el diametro del asteroide:";cin>>setDiametro;
-                cout<<Asteroide1.getDiametro()<<endl;
+                cout<<"El diametro del asteroide introducido es de: "<<setDiametro<<" km.\n"<<endl;
                 }
                 else if(op2==3){
                         break;
@@ -65,18 +66,18 @@ int main(){
         }
         else if (op==2){
                 int op3;
-                cout<<"1)Ingresar datos. \n2) Usar datos predefinidos. \n3)Salir.";cin>>op3;
+                cout<<"1)Usar datos predefinidos. \n2)Ingresar datos. \n3)Salir.\n";cin>>op3;
                 if(op3==1){
-                        cout<<Cometa1.getTamanoCola()<<endl;
-                        cout<<Cometa1.getVelocidadCometa()<<endl;
+                        cout<<"El tamaño de la cola del cometa es: "<<Cometa1.getTamanoCola()<<" km.\n"<<endl;
+                        cout<<"La velocidad del cometa es de: "<<Cometa1.getVelocidadCometa()<<" km/h.\n"<<endl;
                 }
                 else if(op3==2){
                         int setTamanoCola;
                         int setVelocidadCometa;
                         cout<<"Introduce el tamaño de la cola del Cometa:";cin>>setTamanoCola;
-                        cout<<Cometa1.getTamanoCola()<<endl;
+                        cout<<"El tamaño de la cola del cometa introducido es de: "<<setTamanoCola<<" km.\n"<<endl;
                         cout<<"Introduce la velocidad del Cometa:";cin>>setVelocidadCometa;
-                        cout<<Cometa1.getVelocidadCometa()<<endl;
+                        cout<<"La velocidad del cometa introducido es de: "<<setVelocidadCometa<<" km/h.\n"<<endl;
                 }
                 else if(op3==3){
                         break;
@@ -84,12 +85,12 @@ int main(){
         }
         else if (op==3){
                 int op4;
-                cout<<"1)Ingresar datos. \n2) Usar datos predefinidos. \n3)Salir.";cin>>op4;
+                cout<<"1)Usar datos predefinidos. \n2)Ingresar datos. \n3)Salir.\n";cin>>op4;
                 if(op4==1){
-                        cout<<Planeta1.getNumeroLunas()<<endl;
-                        cout<<Planeta1.getDensidad()<<endl;
-                        cout<<Planeta1.getDiametro()<<endl;
-                        cout<<Planeta1.getPeriodoOrbital()<<endl;
+                        cout<<"El planeta tiene: "<<Planeta1.getNumeroLunas()<<" lunas.\n"<<endl;
+                        cout<<"La densidad del planeta es: "<<Planeta1.getDensidad()<<" g/cm3.\n"<<endl;
+                        cout<<"El diametro del planeta es: "<<Planeta1.getDiametro()<<" km.\n"<<endl;
+                        cout<<"El periodo orbital del planeta es de: "<<Planeta1.getPeriodoOrbital()<<" dias.\n"<<endl;
                 }
                 else if(op4==2){
                         int setNumeroLunas;
@@ -97,13 +98,13 @@ int main(){
                         int setDiametro;
                         int setPeriodoOrbital;
                     cout<<"Introduce el número de lunas que tiene el planeta:";cin>>setNumeroLunas;
-                    cout<<Planeta1.getNumeroLunas()<<endl;
+                    cout<<"El planeta introducido tiene: "<<setNumeroLunas<<" lunas.\n"<<endl;
                     cout<<"Introduce la densidad del Planeta:";cin>>setDensidad;
-                    cout<<Planeta1.getDensidad()<<endl;
+                    cout<<"La densidad del planeta introducido es: "<<setDensidad<<" g/cm3.\n"<<endl;
                     cout<<"Introduce el diámetro del Planeta:";cin>>setDiametro;
-                    cout<<Planeta1.getDiametro()<<endl;
+                    cout<<"El diametro del planeta introducido es: "<<setDiametro<<" km.\n"<<endl;
                     cout<<"Introduce el periodo orbital del Planeta:";cin>>setPeriodoOrbital;
-                    cout<<Planeta1.getPeriodoOrbital()<<endl;
+                    cout<<"El periodo orbital del planeta introducido es de: "<<setPeriodoOrbital<<" dias.\n"<<endl;
                 }
                 else if(op4==3){
                         break;
@@ -111,17 +112,17 @@ int main(){
         }
         else if (op==4){
                 int op5;
-                cout<<"1)Ingresar datos. \n2) Usar datos predefinidos. \n3)Salir.";cin>>op5;
+                cout<<"1)Usar datos predefinidos. \n2)Ingresar datos. \n3)Salir.\n";cin>>op5;
                 if(op5==1){
-                        cout<<Luna1.getDiametro()<<endl;
-                        cout<<Luna2.getDiametro()<<endl;
+                        cout<<"El diametro de la luna es de: "<<Luna1.getDiametro()<<" km.\n"<<endl;
+                        cout<<"El diametro de la segunda luna es de: "<<Luna2.getDiametro()<<" km.\n"<<endl;
                 }
                 else if(op5==2){
                         int setDiametro;
                         cout<<"Introduce el diámetro de la Luna1:";cin>>setDiametro;
-                        cout<<Luna1.getDiametro()<<endl;
+                        cout<<"El diametro de la primer luna introducida es de: "<<setDiametro<<" km\n"<<endl;
                         cout<<"Introduce el diámetro de la Luna2:";cin>>setDiametro;
-                        cout<<Luna2.getDiametro()<<endl;
+                        cout<<"El diametro de la segunda luna introducida es de: "<<setDiametro<<" km\n"<<endl;
                 }
                 else if(op5==3){
                         break;
@@ -135,15 +136,3 @@ int main(){
 
     return 0;
 };
-    //cout<<Cuerpo_Celeste1.getTipoMovimiento()<<endl;
-    //cout<<Cuerpo_Celeste1.getOrbitaCuerpo()<<endl;
-//cout<<Cuerpo_Celeste1.getVisibles()<<endl;
-    //cout<<Asteroide1.getDiametro()<<endl;
-    //cout<<Cometa1.getTamanoCola()<<endl;
-    //cout<<Cometa1.getVelocidadCometa()<<endl;
-    //cout<<Planeta1.getDensidad()<<endl;
-    //cout<<Planeta1.getDiametro()<<endl;
-    //cout<<Planeta1.getNumeroLunas()<<endl;
-    //cout<<Planeta1.getPeriodoOrbital()<<endl;
-    //cout<<Luna1.getDiametro()<<endl;
-    //cout<<Luna2.getDiametro()<<endl;
