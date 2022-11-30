ff518e2ed86cbae658cf97c4dbf83a222f364dd0
SebastianCas78 <117695171+SebastianCas78@users.noreply.github.com>
2022-11-30 10:22:30 -0600

    Update Cuerpo_Celeste.cpp

--- Cuerpo_Celeste.cpp
+++ Cuerpo_Celeste.cpp
@@ -1,47 +1,39 @@
-#include "Cuerpo_Celeste.h"
+#include "Cuerpo_Celeste.h" //Se declaran las unidades de los atributos de la clase padre.
 #include <sstream>
 #include "string.h"
 
 Cuerpo_Celeste::Cuerpo_Celeste(){
-    tipoMovimiento=true;
-    orbitaCuerpo=true;
-    visibles=true;
+    tipoMovimiento=0;
+    orbitaCuerpo=0;
+    visibles=0;
 }
 
-Cuerpo_Celeste::Cuerpo_Celeste(bool _tipo_Movimiento, bool _orbita_Cuerpo, bool _visibles){
+Cuerpo_Celeste::Cuerpo_Celeste(int _tipo_Movimiento, int _orbita_Cuerpo, int _visibles){
     tipoMovimiento=_tipo_Movimiento;
     orbitaCuerpo=_orbita_Cuerpo;
     visibles=_visibles;
 }
 
-void Cuerpo_Celeste::setTipoMovimiento(bool _tipo_Movimiento){
+void Cuerpo_Celeste::setTipoMovimiento(int _tipo_Movimiento){
     tipoMovimiento=_tipo_Movimiento;
 }
 
-void Cuerpo_Celeste::setOrbitaCuerpo(bool _orbita_Cuerpo){
+void Cuerpo_Celeste::setOrbitaCuerpo(int _orbita_Cuerpo){
     orbitaCuerpo=_orbita_Cuerpo;
 }
 
-void Cuerpo_Celeste::setVisibles(bool _visibles){
+void Cuerpo_Celeste::setVisibles(int _visibles){
     visibles=_visibles;
 }
 
-bool Cuerpo_Celeste::getTipoMovimiento(){
+int Cuerpo_Celeste::getTipoMovimiento(){
     return tipoMovimiento;
 }
 
-bool Cuerpo_Celeste::getOrbitaCuerpo(){
+int Cuerpo_Celeste::getOrbitaCuerpo(){
     return orbitaCuerpo;
 }
 
-bool Cuerpo_Celeste::getVisibles(){
+int Cuerpo_Celeste::getVisibles(){
     return visibles;
 }
-
-//string Cuerpo_Celeste::imprimeDatos(){
-    //stringstream aux;
-    //aux<<"TipoMovimiento: "<<tipo_movimiento<<endl;
-    //aux<<"OrbitaCuerpo: "<<orbita_cuerpo<<endl;
-    //aux<<"Visibles: "<<visibles<<endl;
-    //return aux.str();
-//}
