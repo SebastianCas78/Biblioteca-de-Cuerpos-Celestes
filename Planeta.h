87a286526f765a86315f52fe3a79086afb66dc7f
SebastianCas78 <117695171+SebastianCas78@users.noreply.github.com>
2022-11-30 10:14:12 -0600

    Update Planeta.h

--- Planeta.h
+++ Planeta.h
@@ -1,4 +1,4 @@
-#include "Luna.cpp" //Se declara la clase Planeta, se manda llamar la clase Luna y sus atributos densidad, diámetro y periodo orbital
+#include "Luna.cpp" //Se declara la clase Planeta, se manda llamar la clase Luna y sus atributos densidad, diámetro y periodo orbital.
 #include "string.h"
 #include "Cuerpo_Celeste.h"
 using namespace std;
@@ -12,7 +12,7 @@ class Planeta : public Cuerpo_Celeste{
 
     public: //Metodos
         Planeta();
-        Planeta(bool, bool, bool,int, float, float, int);
+        Planeta(int, int, int, int, float, float, int);
 
         //Setters
         void setNumeroLunas(int);
