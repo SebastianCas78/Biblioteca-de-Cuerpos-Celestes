613dbee1bfc9f7c605e68aa712614ebe8533b7d4
SebastianCas78 <117695171+SebastianCas78@users.noreply.github.com>
2022-11-30 10:33:45 -0600

    Update Administrador_de_Archivo.cpp

--- Administrador_de_Archivo.cpp
+++ Administrador_de_Archivo.cpp
@@ -1,4 +1,4 @@
-#include "Administrador_de_Archivo.h" //Se definen las funciones de la clase Administrador de Archivo.
+#include "Administrador_de_Archivo.h" //Se define la funcion de la clase Administrador de Archivo.
 #include <sstream>
 #include "string.h"
 
